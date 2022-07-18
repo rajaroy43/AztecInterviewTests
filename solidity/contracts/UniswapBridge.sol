cf3cbce39c88a75fae51b2789cd5767751555e75
rajaroy43 <rajaroy674@gmail.com>
2022-07-18 19:23:08 +0530

    option1 completed

--- solidity/contracts/UniswapBridge.sol
+++ solidity/contracts/UniswapBridge.sol
@@ -8,7 +8,7 @@ import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 
 import {UniswapV2Library} from '@uniswap/v2-periphery/contracts/libraries/UniswapV2Library.sol';
 import {IUniswapV2Router02} from '@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol';
-
+import {IUniswapV2Factory} from "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
 import {IDefiBridge} from './interfaces/IDefiBridge.sol';
 import {Types} from './Types.sol';
 
@@ -32,7 +32,7 @@ contract UniswapBridge is IDefiBridge {
 
     function convert(
         Types.AztecAsset[4] calldata assets,
-        uint64,
+        uint64 auxData,
         uint256,
         uint256 inputValue
     )
@@ -46,32 +46,58 @@ contract UniswapBridge is IDefiBridge {
         )
     {
         require(msg.sender == defiBridgeProxy, 'UniswapBridge: INVALID_CALLER');
-        isAsync = false;
-        uint256[] memory amounts;
-        uint256 deadline = block.timestamp;
-        // TODO This should check the pair exists on UNISWAP instead of blindly trying to swap.
+        require(auxData <= 1000,"UniswapBridge: Invalid_AuxData");
+        // DONE This should check the pair exists on UNISWAP instead of blindly trying to swap.
 
+        address[] memory path = new address[](2);
+        uint amountOut;
         if (assets[0].assetType == Types.AztecAssetType.ETH && assets[2].assetType == Types.AztecAssetType.ERC20) {
-            address[] memory path = new address[](2);
             path[0] = weth;
             path[1] = assets[2].erc20Address;
-            amounts = router.swapExactETHForTokens{value: inputValue}(0, path, defiBridgeProxy, deadline);
-            outputValueA = amounts[1];
+            if(!checkPair(path[0],path[1])){
+                return 
+            }
+            amountOut = router.getAmountsOut(inputValue, path)[1];
+            // Assuming 0 fee for swapping(otherwise we will multiply 997 and then divide by 1000)
+            // Slipage is in 0.1 then auxData will be 100
+            amountOut = amountOut - (amountOut * auxData) /1000
+            outputValueA = router.swapExactETHForTokens{value: inputValue}(amountOut, path, defiBridgeProxy, block.timestamp)[1];
+            isAsync = true;
         } else if (
             assets[0].assetType == Types.AztecAssetType.ERC20 && assets[2].assetType == Types.AztecAssetType.ETH
         ) {
-            address[] memory path = new address[](2);
             path[0] = assets[0].erc20Address;
             path[1] = weth;
+            if(!checkPair(path[0],path[1])){
+                return 
+            }
+            amountOut = router.getAmountsOut(inputValue, path)[1];
+
+            amountOut = amountOut - (amountOut * auxData) /1000
             require(
                 IERC20(assets[0].erc20Address).approve(address(router), inputValue),
                 'UniswapBridge: APPROVE_FAILED'
             );
-            amounts = router.swapExactTokensForETH(inputValue, 0, path, defiBridgeProxy, deadline);
-            outputValueA = amounts[1];
+            outputValueA = router.swapExactTokensForETH(inputValue, amountOut, path, defiBridgeProxy, block.timestamp)[1];
+            isAsync = true;
         } else {
             // TODO what about swapping tokens?
-            revert('UniswapBridge: INCOMPATIBLE_ASSET_PAIR');
+
+            path[0] = assets[0].erc20Address;
+            path[1] = assets[2].erc20Address;
+            if(!checkPair(path[0],path[1])){
+                return 
+            }
+            require(
+                IERC20(path[0]).approve(address(router), inputValue),
+                'UniswapBridge: APPROVE_FAILED'
+            );
+            amountOut = router.getAmountsOut(inputValue, path)[1];
+            // Assuming 0 fee for swapping(otherwise we will multiply 997 and then divide by 1000)
+            // Slipage is in 0.1 then auxData will be 100
+            amountOut = amountOut - (amountOut * auxData) /1000
+            outputValueA = router.swapExactTokensForTokens(inputValue, amountOut, path, defiBridgeProxy, block.timestamp)[1];
+            isAsync = true;
         }
     }
 
@@ -81,6 +107,13 @@ contract UniswapBridge is IDefiBridge {
         uint256
     ) external view override returns (bool) {
         return false;
+    }
+
+    function checkPair(address token0,address token1) internal view returns(bool){
+        address pair= IUniswapV2Factory(router.factory()).getPair(token0,token1);
+        if(pair != address(0)){
+            return true;
+        }
     }
 
     function finalise(
